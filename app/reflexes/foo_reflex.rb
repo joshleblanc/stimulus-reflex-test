b2efccd64250f45ec15a0f0ed8ddae964ecb7f72
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-28 19:42:03 -0300

    session loss example

--- app/reflexes/foo_reflex.rb
+++ app/reflexes/foo_reflex.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class FooReflex < ApplicationReflex
+  before_reflex do |reflex|
+    if reflex.method_name == "handle_change"
+      session[:foo].bars[0].name = element[:value]
+      throw :abort
+    end
+  end
+
+  def handle_change
+    # stub
+  end
+
   def add_bar
     session[:foo].bars.build
   end
